254b14658cacdc6a673d931eda51fdbf7a0e1020
Preet Patel <preetpatel1862@gmail.com>
2025-02-19 20:34:40 -0500

    vercel analysis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Analytics />
     </div>
   );
 }
